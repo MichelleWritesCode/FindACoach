f2f4621d918c872e14718e3a842a9c9d43162efe
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-04-06 14:54:20 +0200

    chore: set up a basic page

---
+++ src/pages/requests/RequestsList.vue
@@ -0,0 +1,11 @@
+<template>
+  <the-title>Requests</the-title>
+</template>
+
+<script>
+import TheTitle from '../../components/layout/TheTitle.vue';
+
+export default {
+  components: { TheTitle },
+};
+</script>
