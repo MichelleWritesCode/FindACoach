1c766d9f04c0d68e4cca05fccc509f6625f584d6
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-04-06 14:53:39 +0200

    chore: create title component

---
+++ src/components/layout/TheTitle.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="title">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  font-size: 24px;
+  font-family: Cochin, Georgia, Times, 'Times New Roman', serif;
+  padding: 60px 0 0 210px;
+  color: rgb(25, 32, 65);
+}
+</style>
