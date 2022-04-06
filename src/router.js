223808f94e1675d4351fdf00553e988041442777
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-04-06 14:50:53 +0200

    chore: set up routes

---
+++ src/router.js
@@ -0,0 +1,26 @@
+import { createWebHistory, createRouter } from 'vue-router'; // use {} because these dependencies are from 'outside' (extern)
+
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachItem from './pages/coaches/CoachItem.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsList from './pages/requests/RequestsList.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coach/:id',
+      component: CoachItem,
+      children: [{ path: 'contact', component: ContactCoach }],
+    },
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestsList },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+});
+
+export default router;
