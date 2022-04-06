fab5f659d6b5161893d63eb6996bc94475c11b72
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-04-06 16:12:54 +0200

    chore: set up basic page

---
+++ src/pages/coaches/CoachRegistration.vue
@@ -0,0 +1,11 @@
+<template>
+  <the-title>Register as a coach</the-title>
+</template>
+
+<script>
+import TheTitle from '../../components/layout/TheTitle.vue';
+
+export default {
+  components: { TheTitle },
+};
+</script>
---
+++ src/pages/requests/ContactCoach.vue
@@ -0,0 +1,11 @@
+<template>
+  <the-title>Contact a coach</the-title>
+</template>
+
+<script>
+import TheTitle from '../../components/layout/TheTitle.vue';
+
+export default {
+  components: { TheTitle },
+};
+</script>
