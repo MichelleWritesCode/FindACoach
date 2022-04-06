73ff481f1134ce452e4b826ed1c4c2e52d7fc7ac
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-04-06 14:52:56 +0200

    chore: create base card

---
+++ src/components/layout/BaseCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="card">
+    <slot></slot>
+  </section>
+</template>
+
+<style scoped>
+.card {
+  border-radius: 6px;
+  background-color: white;
+  box-shadow: 5px 5px 5px -1px rgba(0, 0, 0, 0.05);
+  margin: 20px 0 0 210px;
+  padding: 20px;
+  max-width: 800px;
+}
+</style>
