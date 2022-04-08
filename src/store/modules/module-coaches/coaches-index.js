6cbd4553f8b1f87a0e535f09b128502d868c85bb
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-04-08 15:20:26 +0200

    chore: add data

--- src/store/modules/module-coaches/coaches-index.js
+++ src/store/modules/module-coaches/coaches-index.js
@@ -11,7 +11,7 @@ export default {
           id: 'c1',
           firstName: 'Maximilian',
           lastName: 'Schwarzmüller',
-          areas: ['frontend', 'backend', 'career'],
+          areas: ['front-end', 'back-end', 'career'],
           description:
             "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
           hourlyRate: 30,
@@ -20,10 +20,28 @@ export default {
           id: 'c2',
           firstName: 'Julie',
           lastName: 'Jones',
-          areas: ['frontend', 'career'],
+          areas: ['front-end', 'career'],
           description:
             'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
           hourlyRate: 30,
+        },
+        {
+          id: 'c3',
+          firstName: 'Michelle',
+          lastName: 'Ernst - van Wijk',
+          areas: ['front-end', 'graphic design', 'communications'],
+          description:
+            "I am Michelle and I'm a junior developer in a little company. I build apps for social organisations.",
+          hourlyRate: 110,
+        },
+        {
+          id: 'c4',
+          firstName: 'Ruben',
+          lastName: 'Ernst',
+          areas: ['back-end', 'frond-end', 'migration'],
+          description:
+            "I am Ruben and I'm a senior software developer and I work as a consultant at a bank.",
+          hourlyRate: 90,
         },
       ],
     };
