3177d949290e6f1bfe188284f36b07ec02ae2a59
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-04-08 15:20:10 +0200

    chore: new component

---
+++ src/pages/coaches/CoachDetails.vue
@@ -0,0 +1,11 @@
+<template>
+  <the-title>Coach details</the-title>
+</template>
+
+<script>
+import TheTitle from '../../components/layout/TheTitle.vue';
+
+export default {
+  components: { TheTitle },
+};
+</script>
