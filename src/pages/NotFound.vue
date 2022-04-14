122692f5f13aeef6b3810f8abea7c703cbb252a1
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-04-14 10:30:48 +0200

    chore: create basic not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <the-title>PAGE NOT FOUND</the-title>
+</template>
+
+<script>
+import TheTitle from '../components/layout/TheTitle.vue';
+
+export default {
+  components: { TheTitle },
+};
+</script>
