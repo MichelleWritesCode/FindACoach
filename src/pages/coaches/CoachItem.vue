80bb375fb41f1b64a7e5a4032340966f6499739e
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-04-14 10:32:38 +0200

    feat: create functionality of coach item

--- src/pages/coaches/CoachItem.vue
+++ src/pages/coaches/CoachItem.vue
@@ -1,20 +1,39 @@
 <template>
-  <li>
-    <!-- <slot name="coachName"></slot>
+  <section class="inline">
+    <li>
+      <!-- <slot name="coachName"></slot>
     <slot name="coachAreas"></slot>
     <slot name="coachDescription"></slot>
     <slot name="coachHourlyRate"></slot> -->
 
-    <h3>{{ coachFirstName }} {{ coachLastName }}</h3>
-    <p class="areas">{{ areaStyling(coachAreas) }}</p>
-    <p>{{ coachDescription }}</p>
-    <p class="hourlyRate">Hourly rate: $ {{ coachHourlyRate }},-</p>
-  </li>
+      <h3>{{ fullName }}</h3>
+      <p class="areas">{{ areaStyling(coachAreas) }}</p>
+      <!-- <p>{{ coachDescription }}</p> -->
+      <p class="hourlyRate">$ {{ coachHourlyRate }},- / hour</p>
+    </li>
+    <div class="actions">
+      <ul>
+        <li>
+          <router-link :to="coachDetailsLink" class="link">details</router-link>
+        </li>
+        <li>
+          <router-link :to="coachContactLink" class="link">contact</router-link>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
 export default {
   props: {
+    coachId: {
+      type: String,
+      required: true,
+    },
     coachFirstName: {
       type: String,
       required: true,
@@ -36,21 +55,38 @@ export default {
       required: true,
     },
   },
-  setup() {
+  setup(props) {
+    const route = useRoute();
+
+    const fullName = computed(() => {
+      return props.coachFirstName + ' ' + props.coachLastName;
+    });
+
+    const coachDetailsLink = computed(() => {
+      return route.path + '/' + props.coachId;
+    });
+
+    const coachContactLink = computed(() => {
+      return route.path + '/' + props.coachId + '/contact';
+    });
+
     function areaStyling(input) {
       input = input.join(' | ');
       return input;
     }
 
-    return { areaStyling };
+    return { areaStyling, coachContactLink, coachDetailsLink, fullName };
   },
 };
 </script>
 
 <style scoped>
+ul {
+  text-align: right;
+}
+
 li {
   list-style: none;
-  border-bottom: 1px solid goldenrod;
   padding-bottom: 6px;
 }
 
@@ -59,14 +95,42 @@ p.areas {
   color: rgb(25, 32, 65);
 }
 
+a {
+  text-decoration: none;
+  color: goldenrod;
+}
+
+a:hover {
+  /* background-color: goldenrod;
+  color: white;
+  padding: 5px 15px; */
+  border-bottom: rgb(25, 32, 65) solid 1.5px;
+}
+
+a.link {
+  font-size: 14px;
+}
+
+div.actions {
+  margin-top: 30px;
+}
+
 .areas {
   font-style: italic;
-  font-size: 12px;
+  font-size: 14px;
 }
 
 .hourlyRate {
-  color: rgb(25, 32, 65, 0.6);
-  font-style: italic;
-  font-size: 12px;
+  color: rgb(25, 32, 65, 0.75);
+  font-weight: 600;
+  font-size: 14px;
+}
+
+.inline {
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  border-bottom: 1px solid rgb(25, 32, 65, 0.1);
+  margin-right: 20px;
 }
 </style>
