aadfa03f2e9e0045127a3a3df183f3143929bf4f
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-04-14 10:31:11 +0200

    chore: create button component

---
+++ src/components/layout/BaseButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+button {
+  background-color: goldenrod;
+  color: white;
+  border: none;
+  padding: 5px 15px;
+}
+
+button:hover {
+  border-bottom: 3px solid rgb(25, 32, 65);
+}
+</style>
