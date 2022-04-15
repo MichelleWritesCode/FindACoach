bce1a6a6fc8187bba161260e85a7780ec1f5adf1
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-04-15 14:44:51 +0200

    chore: create button with outline

---
+++ src/components/layout/BaseButtonOutline.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+button {
+  background-color: white;
+  border: solid 1px goldenrod;
+  color: white;
+  border: none;
+  font-size: 16px;
+}
+</style>
