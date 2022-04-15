8b6a79bc3277a27a4659c1e984de86e485a962df
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-04-15 11:40:45 +0200

    refactor: clean code

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -4,8 +4,12 @@
     <section class="toolbar">
       <section class="filter"><h2>Filter</h2></section>
       <section class="controle">
-        <button>refresh</button>
-        <button><router-link to="/register">register</router-link></button>
+        <base-button><template v-slot>refresh</template></base-button>
+        <base-button
+          ><template v-slot
+            ><router-link to="/register">register</router-link></template
+          ></base-button
+        >
       </section>
     </section>
   </base-card>
@@ -13,11 +17,13 @@
   <base-card>
     <section>
       <h2>List of coaches</h2>
+      <hr />
       <section>
-        <ul>
+        <ul v-if="hasCoaches">
           <coach-item
             v-for="coach in filteredCoaches"
             :key="coach.id"
+            :coachId="coach.id"
             :coachFirstName="coach.firstName"
             :coachLastName="coach.lastName"
             :coachDescription="coach.description"
@@ -26,12 +32,14 @@
           >
           </coach-item>
         </ul>
+        <h3 v-else>No coaches found</h3>
       </section>
     </section>
   </base-card>
 </template>
 
 <script>
+import BaseButton from '../../components/layout/BaseButton.vue';
 import BaseCard from '../../components/layout/BaseCard.vue';
 import TheTitle from '../../components/layout/TheTitle.vue';
 import CoachItem from './CoachItem.vue';
@@ -40,14 +48,19 @@ import { computed } from 'vue';
 import { useStore } from 'vuex';
 
 export default {
-  components: { BaseCard, TheTitle, CoachItem },
+  components: { BaseButton, BaseCard, TheTitle, CoachItem },
   setup() {
     const store = useStore();
+
+    const hasCoaches = computed(() => {
+      return store.getters['allCoaches/hasCoaches'];
+    });
+
     const filteredCoaches = computed(() => {
       return store.getters['allCoaches/coaches']; //allCoaches is the namespace of the module-coaches and coaches is the name of the getter (take a look a coaches-getters)
     });
 
-    return { filteredCoaches };
+    return { filteredCoaches, hasCoaches };
   },
 };
 </script>
@@ -57,8 +70,16 @@ h2 {
   margin-bottom: 30px;
 }
 
+hr {
+  border: 0;
+  border-top: 1px solid rgb(25, 32, 65, 0.1);
+  margin-right: 20px;
+  margin-bottom: 0;
+}
+
 ul {
   padding-left: 0;
+  margin-top: 0;
 }
 
 .toolbar .filter {
