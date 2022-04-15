e25a3d4def658b21d37a9aef8c503b63af8cba34
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-04-15 16:57:02 +0200

    feat: create BaseRibbon component

---
+++ src/components/layout/BaseRibbon.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="ribbon">
+    <slot></slot>
+  </section>
+</template>
+
+<style scoped>
+.ribbon {
+  background-color: rgb(25, 32, 65, 0.1);
+  color: white;
+  border: none;
+  padding: 5px 5px;
+  font-size: 16px;
+  width: 110px;
+  clip-path: polygon(100% 0, 95% 50%, 100% 100%, 0% 100%, 0% 50%, 0% 0%);
+}
+</style>
