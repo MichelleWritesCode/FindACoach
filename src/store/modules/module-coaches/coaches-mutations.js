3a15dc2afe9a65751677abbeb1d0ecd29db99402
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-05-05 10:47:55 +0200

    add comment for learning Vue

--- src/store/modules/module-coaches/coaches-mutations.js
+++ src/store/modules/module-coaches/coaches-mutations.js
@@ -1,5 +1,6 @@
 export default {
   registerCoach(state, payload) {
+    //payload is the formData which comes from CoachForm.vue via CoachRegistration.vue
     state.coaches.push(payload);
   },
 };
