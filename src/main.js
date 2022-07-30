31697134536186d526cd539981d77a531ab16dd1
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-07-30 13:21:53 +0200

    chore: import fontAwesomeIcon into the app

--- src/main.js
+++ src/main.js
@@ -4,9 +4,17 @@ import App from './App.vue';
 import router from './router.js';
 import store from './store/index.js';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas);
+
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app');
