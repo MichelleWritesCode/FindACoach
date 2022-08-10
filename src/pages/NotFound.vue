bb9ae5496c92aa0c2170d37c6ac677fbb79dad0b
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-08-10 15:50:29 +0200

    feat: create not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,11 +1,27 @@
 <template>
-  <the-title>PAGE NOT FOUND</the-title>
+  <section class="container">
+    <the-title>Page not found!</the-title>
+    <base-card>
+      <p>Sorry, we couldn't found the page you were looking for.</p>
+      <base-button class="button">
+        <router-link to="/">Back to the homepage</router-link>
+      </base-button>
+    </base-card>
+  </section>
 </template>
 
 <script>
 import TheTitle from '../components/layout/TheTitle.vue';
+import BaseCard from '../components/ui/BaseCard.vue';
+import BaseButton from '../components/ui/BaseButton.vue';
 
 export default {
-  components: { TheTitle },
+  components: { TheTitle, BaseCard, BaseButton },
 };
 </script>
+
+<style>
+.button {
+  margin-top: 50px;
+}
+</style>
