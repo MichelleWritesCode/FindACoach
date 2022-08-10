6071ebfc7191ff075ee14002eacb997e30320404
MichelleWritesCode <mernst@derealisatiegroep.nl>
2022-08-10 15:53:15 +0200

    feat: show requests, error handling and data traffic requests

--- src/pages/requests/RequestsList.vue
+++ src/pages/requests/RequestsList.vue
@@ -1,4 +1,10 @@
 <template>
+  <base-dialog
+    :showDialog="!!error"
+    title="An error has occurred!"
+    @close="closeDialog"
+    ><p>{{ error }}</p></base-dialog
+  >
   <section class="container">
     <the-title>Requests received</the-title>
     <section>
@@ -22,23 +28,45 @@ import BaseCard from '../../components/ui/BaseCard.vue';
 import TheTitle from '../../components/layout/TheTitle.vue';
 import RequestItem from '../../components/requests/RequestItem.vue';
 
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
 import { useStore } from 'vuex';
+import BaseDialog from '../../components/ui/BaseDialog.vue';
 
 export default {
-  components: { BaseCard, RequestItem, TheTitle },
+  components: { BaseCard, RequestItem, TheTitle, BaseDialog },
   setup() {
     const store = useStore();
+
     const hasRequests = computed(() => {
       return store.getters['allRequests/hasRequests'];
     });
+
     const receivedRequests = computed(() => {
       return store.getters['allRequests/requests'];
     });
 
+    const error = ref(null);
+
+    function closeDialog() {
+      error.value = null;
+    }
+
+    async function loadRequests() {
+      try {
+        await store.dispatch('allRequests/loadRequests');
+      } catch (catchedError) {
+        error.value = catchedError.message || 'Something went wrong';
+      }
+    }
+
+    loadRequests();
+
     return {
+      closeDialog,
+      error,
       hasRequests,
+      loadRequests,
       receivedRequests,
     };
   },
--- src/store/modules/module-requests/requests-index.js
+++ src/store/modules/module-requests/requests-index.js
@@ -6,20 +6,7 @@ export default {
   namespaced: true,
   state() {
     return {
-      requests: [
-        // {
-        //   coachId: 'c1',
-        //   userEmail: 'requestformichelle@udemy.com',
-        //   message:
-        //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        // },
-        // {
-        //   coachId: 'c2',
-        //   userEmail: 'requestforruben@udemy.com',
-        //   message:
-        //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        // },
-      ],
+      requests: [],
     };
   },
   actions: requestsActions,
